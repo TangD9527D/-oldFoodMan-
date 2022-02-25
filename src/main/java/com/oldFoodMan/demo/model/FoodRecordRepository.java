086c95dd05bb89df630c6a89408e0e8260ce2816
Lynn <akimoto113915@gmail.com>
2022-02-25 11:55:28 +0800

    Lynn-新增2個model ( FoodRecord & FoodRecordRepository )

---
+++ src/main/java/com/oldFoodMan/demo/model/FoodRecordRepository.java
@@ -0,0 +1,14 @@
+package com.oldFoodMan.demo.model;
+
+
+
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface FoodRecordRepository extends JpaRepository<FoodRecord,Integer> {
+
+	public FoodRecord findFirstByOrderByAddedDesc();
+	
+}
