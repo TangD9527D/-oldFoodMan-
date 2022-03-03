bab00aeca82fb6ce024233a12b37fb6702cf7bbf
Eddie <begg0425@gmail.com>
2022-03-03 10:55:44 +0800

    虛驚一場

--- src/main/java/com/oldFoodMan/demo/model/ScheduleBean.java
+++ src/main/java/com/oldFoodMan/demo/model/ScheduleBean.java
@@ -1,11 +1,14 @@
 package com.oldFoodMan.demo.model;
 
 import javax.persistence.Column;
+import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
 
+@Entity
 public class ScheduleBean {
+	
 	
 	@Id
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
---
+++ src/main/java/com/oldFoodMan/demo/service/ScheduleService.java
@@ -0,0 +1,69 @@
+package com.oldFoodMan.demo.service;
+
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Sort;
+import org.springframework.stereotype.Service;
+
+import com.oldFoodMan.demo.model.ScheduleBean;
+import com.oldFoodMan.demo.model.ScheduleRepository;
+
+
+
+@Service
+public class ScheduleService {
+	
+	@Autowired
+	private ScheduleRepository sbdao ;
+	
+	public void insert (ScheduleBean sb ) {
+		
+		sbdao.save(sb);		
+		
+	}
+	
+	public ScheduleBean findById(Integer id) {		
+		
+		Optional<ScheduleBean> op=sbdao.findById(id);
+		
+		if(op.isPresent()) {
+			
+			return op.get();
+		}
+		return null ;
+		
+	}
+	
+	public void delete (Integer id) {
+		
+		sbdao.deleteById(id);		
+		
+	}
+	
+	public List<ScheduleBean> findAll(){
+		
+		List<ScheduleBean> location=sbdao.findAll();
+		
+		return location ;
+		
+	}
+	
+	public Page<ScheduleBean> findByPage(Integer pageNumber){
+		
+		
+		Pageable pgb=PageRequest.of(pageNumber-1, 8,Sort.Direction.DESC,"added");
+		
+		return sbdao.findAll(pgb);
+		
+		
+		
+		
+	}
+	
+
+}
