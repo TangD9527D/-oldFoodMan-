2c31c69468b882339e61e5acc188d73c7acbb2bc
Eddie <begg0425@gmail.com>
2022-03-04 17:12:30 +0800

    憨

--- src/main/java/com/oldFoodMan/demo/controller/ColletContrller.java
+++ src/main/java/com/oldFoodMan/demo/controller/ColletContrller.java
@@ -4,8 +4,12 @@ import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.servlet.ModelAndView;
 
+import com.oldFoodMan.demo.dto.ColletDto;
 import com.oldFoodMan.demo.model.FoodRecord;
 import com.oldFoodMan.demo.service.ColletService;
 
@@ -13,22 +17,20 @@ import com.oldFoodMan.demo.service.ColletService;
 public class ColletContrller {
 	
 	@Autowired
-	ColletService serivce;
-		
-	public List<FoodRecord> colletRecord(ModelAndView mav,String shopType) {
-		
-		
-		
-		
-		
-		
-		
-		
-		return null;
+	ColletService service;
 	
-	
-	
+	@ResponseBody
+	@GetMapping("/api/collet")
+	public List<FoodRecord> colletRecord(ModelAndView mav,@RequestBody ColletDto dto) {
 		
+		System.out.println("shopType:"+dto.getLikeLocations());
+//		mav.setViewName("maps");
+		
+		List<FoodRecord> st =service.findByShopType(dto.getLikeLocations());
+			
+		mav.getModel().put("st", st);		
+		
+		return st ;
 		
 	}
 	
