cb8f8468185879b748bfe4959e486b9bce85e4e4
Jack <c7416706@gmail.com>
2022-03-14 17:27:51 +0800

    Jack0314

--- src/main/java/com/oldFoodMan/demo/model/CouponRepository.java
+++ src/main/java/com/oldFoodMan/demo/model/CouponRepository.java
@@ -1,7 +1,10 @@
 package com.oldFoodMan.demo.model;
 
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface CouponRepository extends JpaRepository<Coupon, Integer> {
-
+	
+	public List<Coupon> findByCouponId(int couponId);
 }
